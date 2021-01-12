3ece3202cda384bb8076fc691724c8883688c79d
Illia Soroka <ilycha92@gmail.com>
2021-01-12 12:55:15 -0600

    bug fixed sidebar

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,13 @@
 const burger = document.querySelector('.header_burger');
 const sidebar = document.querySelector('.sidebar_roll');
 const li = document.querySelectorAll('.link');
-
 burger.addEventListener('click', () => {
-  if (sidebar.style.display == 'none') {
-    sidebar.style.display = 'block';
+  if (sidebar.classList) {
+    sidebar.classList.toggle('sidebar')
     for (let i = 0; i < li.length; i++) {
       li[i].addEventListener('click', () => {
-        if (sidebar.style.display == 'block')
-          sidebar.style.display = 'none';
+        sidebar.classList.remove('sidebar')
       })
     }
-  } else {
-    sidebar.style.display = 'none';
   }
 })
